a56b07b190005ac34991c8a7829210fbb77bf391
Tinco Andringa <mail@tinco.nl>
2009-11-23 18:54:21 +0100

    implemented map parsing

--- RProxyBot/map.rb
+++ RProxyBot/map.rb
@@ -7,16 +7,32 @@ module RProxyBot
 		end
 
     def ground_height(x,y)
-      #Staat in data per walk tile van 4x4
-      walk_x = x / 8
-      walk_y = y / 8
+      pos = calc_walk_pos(x,y)
+      #need an enum for this
+      height_data[pos]
     end
 
     def buildable?(x,y)
-      #
+      #is in build_data per build tile of 32x32
+      build_x = x / 32
+      build_y = y / 32
+      pos = (self.width * build_x) + build_y
+      build_data[pos] == "1"
     end
 
     def walkable?(x,y)
+      pos = calc_walk_pos(x,y)
+      walk_data[pos] == "1"
     end
-	end
+
+    private
+    def calc_walk_pos(x,y)
+      #Staat in data per walk tile van 8x8
+      walk_x = x / 8
+      walk_y = y / 8
+      #walk_data is in y stukjes van x
+      #width is in build tiles van 4 walk tiles
+      (self.width * 4 * walk_y) + walk_x
+    end
+  end
 end
